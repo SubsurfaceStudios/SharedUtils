0655c61b9d8e69a0cdfaa70aaf0f46d22638f16a
Raven <raven@az-raven.com>
2023-03-11 17:20:42 -0700

    Static Coroutines: Initial Commit

---
+++ SubsurfaceStudios/StaticCoroutineHandler.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+namespace SubsurfaceStudios.Utilities.Async {
+
+    public class StaticCoroutineHandler : MonoBehaviour
+    {
+        private static StaticCoroutineHandler Instance;
+
+        void Awake() {
+            Instance = this;
+            DontDestroyOnLoad(transform.root);
+        }
+        public static Coroutine StartCoroutineStatic(IEnumerator routine) => Instance.StartCoroutine(routine);
+        public static void StopCoroutineStatic(Coroutine routine) => Instance.StopCoroutine(routine);
+        public static void StopCoroutineStatic(IEnumerator routine) => Instance.StopCoroutine(routine);
+        public static void StopCoroutineStatic(string routine_name) => Instance.StopCoroutine(routine_name);
+        public static void StopAllCoroutinesStatic() => Instance.StopAllCoroutines();
+
+    }
+}
