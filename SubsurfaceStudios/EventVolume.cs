5b786f8c6d87c0434f0741604e27b93deb2dbe5c
Raven <raven@az-raven.com>
2023-03-11 17:22:39 -0700

    Event Volume: Initial Commit

---
+++ SubsurfaceStudios/EventVolume.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SubsurfaceStudios.Utilities.Async {
+
+    public class StaticCoroutineHandler : MonoBehaviour
+    {
+        private static StaticCoroutineHandler Instance;
+
+        void Awake() {
+            Instance = this;
+            DontDestroyOnLoad(transform.root);
+        }
+        public static Coroutine StartCoroutineStatic(IEnumerator routine) => Instance.StartCoroutine(routine);
+        public static void StopCoroutineStatic(Coroutine routine) => Instance.StopCoroutine(routine);
+        public static void StopCoroutineStatic(IEnumerator routine) => Instance.StopCoroutine(routine);
+        public static void StopCoroutineStatic(string routine_name) => Instance.StopCoroutine(routine_name);
+        public static void StopAllCoroutinesStatic() => Instance.StopAllCoroutines();
+
+    }
+}
