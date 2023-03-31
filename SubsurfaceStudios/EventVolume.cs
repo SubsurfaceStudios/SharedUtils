acfaf57b6485b69ec851e441b7d212dd33afb907
Raven <raven@az-raven.com>
2023-03-31 14:37:57 -0700

    Event Volume: Fix mis-clone.
    Signed-off-by: Raven <raven@az-raven.com>

--- SubsurfaceStudios/EventVolume.cs
+++ SubsurfaceStudios/EventVolume.cs
@@ -1,22 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
-namespace SubsurfaceStudios.Utilities.Async {
+namespace SubsurfaceStudios {
+    public sealed class EventVolume : MonoBehaviour {
+        public LayerMask m_PlayerLayerMask;
 
-    public class StaticCoroutineHandler : MonoBehaviour
-    {
-        private static StaticCoroutineHandler Instance;
+        public UnityEvent m_OnPlayerEnter;
+        public UnityEvent m_OnPlayerExit;
 
-        void Awake() {
-            Instance = this;
-            DontDestroyOnLoad(transform.root);
+        public LayerMask m_ObjectLayerMask;
+
+        public UnityEvent m_OnObjectEnter;
+        public UnityEvent m_OnObjectExit;
+
+        void OnTriggerEnter(Collider other) {
+            if (((1 << other.gameObject.layer) & m_PlayerLayerMask.value) > 0) {
+                m_OnPlayerEnter.Invoke();
+            } else if (((1 << other.gameObject.layer) & m_ObjectLayerMask.value) > 0) {
+                m_OnObjectEnter.Invoke();
+            }
         }
-        public static Coroutine StartCoroutineStatic(IEnumerator routine) => Instance.StartCoroutine(routine);
-        public static void StopCoroutineStatic(Coroutine routine) => Instance.StopCoroutine(routine);
-        public static void StopCoroutineStatic(IEnumerator routine) => Instance.StopCoroutine(routine);
-        public static void StopCoroutineStatic(string routine_name) => Instance.StopCoroutine(routine_name);
-        public static void StopAllCoroutinesStatic() => Instance.StopAllCoroutines();
 
+        void OnTriggerExit(Collider other) {
+            if (((1 << other.gameObject.layer) & m_PlayerLayerMask.value) > 0) {
+                m_OnPlayerExit.Invoke();
+            } else if (((1 << other.gameObject.layer) & m_ObjectLayerMask.value) > 0) {
+                m_OnObjectExit.Invoke();
+            }
+        }
     }
 }
