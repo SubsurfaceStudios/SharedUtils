13c4e7d34e58e7cb9a80fb095740868fa15c5850
Raven <raven@az-raven.com>
2023-08-26 16:16:01 -0700

    Memory: Implemented the Bytes utility.
    This utility class allows for blitting structure types to and from an
    opaque array of bytes. In the future, it will likely support further
    manipulation, but for now it is purely an abstraction over C#'s
    System.Runtime.InteropServices.Marshal system.
    
    NOTE: Many of the features used in this class are unsafe or unstable!
    You need to be certain or explicit about the layout of your structures!
    
    Signed-off-by: Raven <raven@az-raven.com>

---
+++ Bytes.cs
@@ -0,0 +1,76 @@
+using System;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+namespace SubsurfaceStudios.Utilities.Memory.Unsafe {
+    /// <summary>
+    /// A collection of relatively-unsafe functions for the manipulation of memory structures.
+    /// </summary>
+    /// <remarks>
+    /// These functions are not particularly safe. Use with caution.
+    /// </remarks>
+	public static class Bytes {
+        /// <summary>
+        /// Copies the memory representation of a struct into a byte array of the same size.
+        /// </summary>
+        /// <remarks>
+        /// This function is dependent on the layout of the structure specified.
+        /// Therefore, this should be used with caution.
+        /// </remarks>
+        /// <param name="value">The struct to copy data from.</param>
+        /// <typeparam name="T">The type of struct to convert from.</typeparam>
+        public static byte[] Of<T>(in T value) where T: struct {
+			int size = Marshal.SizeOf(value);
+            byte[] bytes = new byte[size];
+
+			IntPtr ptr = IntPtr.Zero;
+			try {
+                ptr = Marshal.AllocHGlobal(size);
+                Marshal.StructureToPtr(value, ptr, true);
+                Marshal.Copy(ptr, bytes, 0, size);
+            } catch (Exception ex) {
+				Debug.LogException(ex);
+				return null;
+			} finally {
+				Marshal.FreeHGlobal(ptr);
+			}
+
+			return bytes;
+		}
+
+        /// <summary>
+        /// Converts a properly-sized array of bytes into a struct directly.
+        /// </summary>
+        /// <remarks>
+        /// This function is dependent on the layout of the structure specified.
+        /// Therefore, this should be used with caution.
+        /// </remarks>
+        /// <param name="bytes">The array of bytes to copy data from.</param>
+        /// <typeparam name="T">The type of struct to convert to.</typeparam>
+        /// <returns>A tagged union containing either the converted struct or an error the function encountered.</returns>
+		public static TaggedUnion<T, Exception> To<T>(this byte[] bytes) where T: struct {
+            T value = default;
+
+            int size = Marshal.SizeOf(value);
+            if (size != bytes.Length)
+                return new(new ArgumentException($"The size of the struct ({size}) does not match the size of the byte array ({bytes.Length})."));
+
+            IntPtr ptr = IntPtr.Zero;
+
+            try {
+                ptr = Marshal.AllocHGlobal(size);
+
+                Marshal.Copy(bytes, 0, ptr, size);
+
+                value = Marshal.PtrToStructure<T>(ptr);
+            } catch (Exception ex) {
+                return new(ex);
+            } finally {
+                if (ptr != IntPtr.Zero)
+                    Marshal.FreeHGlobal(ptr);
+            }
+
+			return new(value);
+		}
+	}
+}
