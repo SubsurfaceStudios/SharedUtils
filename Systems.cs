3a5b0b6aae2900573088a9f54789564b290612f5
Raven <raven@az-raven.com>
2023-08-26 18:42:14 -0700

    Systems: Fix formatting in YieldableTask<T>
    Signed-off-by: Raven <raven@az-raven.com>

--- Systems.cs
+++ Systems.cs
@@ -165,16 +165,15 @@ namespace SubsurfaceStudios.Utilities.Async {
     using System.Threading.Tasks;
     using UnityEngine;
 
-    public class YieldableTask<T> : CustomYieldInstruction
-    {
+    public class YieldableTask<T> : CustomYieldInstruction {
         private readonly Task<T> await;
         public YieldableTask(Task<T> await) => this.await = await;
 
         public T result => await.Result;
 
         public override bool keepWaiting => !await.IsCompleted;
+    }
     public class YieldableTask : CustomYieldInstruction {
-    {
         private readonly Task await;
         public YieldableTask(Task await) => this.await = await;
         public override bool keepWaiting => !await.IsCompleted;
