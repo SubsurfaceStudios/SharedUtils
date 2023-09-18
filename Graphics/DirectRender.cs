31550faf79ff0300bb7d07ee4a6b865edca31a1d
Raven <raven@az-raven.com>
2023-09-17 21:25:21 -0700

    DirectRender: Still working to get this stable.
    Signed-off-by: Raven <raven@az-raven.com>

--- Graphics/DirectRender.cs
+++ Graphics/DirectRender.cs
@@ -1,31 +1,88 @@
 using System;
 using UnityEngine;
 
-namespace SubsurfaceStudios.Graphics {
+namespace SubsurfaceStudios.Rendering {
     public static class DirectRender {
-        public static RenderTexture Now(Vector2Int size, Matrix4x4 matrix, Action<RenderTexture> perform_actions) {
-            RenderTexture old = RenderTexture.active;
+        public static readonly Mesh QuadIdentity = new Mesh() {
+            vertices = new Vector3[] {
+                new(0, 0, 0),
+                new(1, 0, 0),
+                new(0, 1, 0),
+                new(1, 1, 0),
+            },
+            triangles = new int[] {
+                0, 2, 1,
+                2, 3, 1,
+            },
+            uv = new Vector2[] {
+                new(0, 0),
+                new(1, 0),
+                new(0, 1),
+                new(1, 1),
+            },
+        };
 
-            RenderTexture rt = new(size.x, size.y, 0);
-            if (!rt.Create()) {
-                rt.Release();
-                return null;
-            };
+        public static readonly Material Unlit = new Material(Shader.Find("Unlit/Transparent"));
 
-            RenderTexture.active = rt;
-            GL.PushMatrix();
-            GL.LoadProjectionMatrix(matrix);
+        public static float GetAspectRatio(this RenderTexture target) {
+            return (float)target.width / (float)target.height;
+        }
 
-            try {
-                perform_actions(rt);
-            } finally {
-                // Clean up after ourselves.
-                GL.PopMatrix();
-                GL.invertCulling = false;
-                RenderTexture.active = old;
-            }
-
-            return rt;
+        public static DirectRenderContext Enter2DRenderingContext(this RenderTexture target) {
+            return new DirectRenderContext(
+                Matrix4x4.Ortho(
+                    // Rect
+                    0, 1, 0, 1, 
+                    
+                    // Near plane
+                    -100,
+                    
+                    // Far plane
+                    100
+                ),
+                target
+            );
         }
     }
+
+	public class DirectRenderContext : IDisposable {
+        public RenderTexture old_rt;
+
+        public DirectRenderContext(Matrix4x4 matrix, RenderTexture target) {
+            old_rt = RenderTexture.active;
+            RenderTexture.active = target;
+
+            GL.PushMatrix();
+            GL.LoadProjectionMatrix(matrix);
+        }
+
+		public void Dispose() {
+            GL.PopMatrix();
+            RenderTexture.active = old_rt;
+		}
+
+        public void DrawRect(Color color, Rect quad) {
+            DrawQuad(Texture2D.whiteTexture, color, quad);
+        }
+
+        public void Clear(Color color) {
+            GL.Clear(true, true, color);
+        }
+
+        public void DrawQuad(Texture tex, Color tint, Rect quad) {
+            Matrix4x4 transform = Matrix4x4.TRS(
+                quad.position,
+                Quaternion.identity,
+                quad.size
+            );
+
+            if (!DirectRender.Unlit.SetPass(0))
+                Debug.LogError("SetPass returned false");
+
+            DirectRender.Unlit.mainTexture = tex;
+            DirectRender.Unlit.color = tint;
+
+            Graphics.DrawMeshNow(DirectRender.QuadIdentity, transform);
+        }
+	}
 }
