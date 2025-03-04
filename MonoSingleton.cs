f05ccbfab9fd2142c3ecfeb3d7bbebc535cd43c9
averyocean65 <111367792+averyocean65@users.noreply.github.com>
2025-03-04 22:07:41 +0100

    Create MonoSingleton.cs (#4)
    * Create MonoSingleton.cs
    
    Signed-off-by: averyocean65 <111367792+averyocean65@users.noreply.github.com>
    
    * Update MonoSingleton.cs
    
    Co-authored-by: Raven <raven@az-raven.com>
    Signed-off-by: averyocean65 <111367792+averyocean65@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: averyocean65 <111367792+averyocean65@users.noreply.github.com>
    Co-authored-by: Raven <raven@az-raven.com>

---
+++ MonoSingleton.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+namespace SubsurfaceStudios.Utilities.Instancing
+{
+    public abstract class MonoSingleton<T> : MonoBehaviour where T : MonoSingleton<T>
+    {
+        public static T Instance { get; private set; }
+
+        protected virtual void Awake()
+        {
+            if (Instance != null && Instance != this)
+            {
+                Destroy(this);
+                return;
+            }
+
+            Instance = (T)this;
+        }
+
+        protected virtual void OnDestroy()
+        {
+            if (Instance == this)
+                Instance = null;
+        }
+    }
+}
